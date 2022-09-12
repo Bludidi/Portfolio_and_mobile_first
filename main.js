6ae48055d758ecdff79455d5c785d7d189fb3c29
NurkaAmre <107000157+NurkaAmre@users.noreply.github.com>
2022-09-12 17:29:57 +0600

    Add updates

--- main.js
+++ main.js
@@ -2,20 +2,20 @@ const menu = document.querySelector('.menu');
 const menuOption = document.querySelector('.menu_opt');
 const closeMenu = document.querySelector('.close_menu');
 
-menu.addEventListener('click', ()=>{
+menu.addEventListener('click', () => {
 menuOption.classList.remove['open'];
 menuOption.style.display='block'; 
 menuOption.style.height='100vh'; 
 menu.style.visibility='hidden';
 closeMenu.style.visibility='visible';
 });
-closeMenu.addEventListener('click', ()=>{
+closeMenu.addEventListener('click', () => {
     menuOption.classList.toggle['open'];
     menuOption.style.height='0';
     menu.style.visibility='visible';
     closeMenu.style.visibility='hidden';
 });
-menuOption.addEventListener('click', ()=>{
+menuOption.addEventListener('click', () => {
     closeMenu.style.visibility='hidden';
     menuOption.style.display='none';
     menuOption.style.height='0'; 
