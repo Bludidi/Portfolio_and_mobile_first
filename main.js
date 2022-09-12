47062302fbea22f042acf12efa1ca7099d352067
Bludidi <bludidi5@gmail.com>
2022-09-12 15:29:27 +0200

    fixed js file

--- main.js
+++ main.js
@@ -3,11 +3,11 @@ const menuOption = document.querySelector('.menu_opt');
 const closeMenu = document.querySelector('.close_menu');
 
 menu.addEventListener('click', () => {
-menuOption.classList.remove.open;
-menuOption.style.display = 'block'; 
-menuOption.style.height = '100vh'; 
-menu.style.visibility = 'hidden';
-closeMenu.style.visibility = 'visible';
+  menuOption.classList.remove.open;
+  menuOption.style.display = 'block';
+  menuOption.style.height = '100vh';
+  menu.style.visibility = 'hidden';
+  closeMenu.style.visibility = 'visible';
 });
 
 closeMenu.addEventListener('click', () => {
