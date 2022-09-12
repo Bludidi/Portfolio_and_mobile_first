d9e09b562361979d94dbbd9abb1a9324d4aadb7c
Bludidi <bludidi5@gmail.com>
2022-09-12 12:44:01 +0200

    Added menu and close menu in the JS code

---
+++ main.js
@@ -0,0 +1,17 @@
+const menu = document.querySelector('.menu');
+const menuOption = document.querySelector('.menu_opt');
+const closeMenu = document.querySelector('.close_menu');
+
+menu.addEventListener('click', ()=>{
+menuOption.classList.remove['open'];
+menuOption.style.display='block'; 
+menuOption.style.height='100vh'; 
+menu.style.visibility='hidden';
+closeMenu.style.visibility='visible';
+});
+closeMenu.addEventListener('click', ()=>{
+    menuOption.classList.toggle['open'];
+    menuOption.style.height='0';
+    menu.style.visibility='visible';
+    closeMenu.style.visibility='hidden';
+});
