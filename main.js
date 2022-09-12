82033a445eba78a936a1800803b02d29228e15c2
Bludidi <bludidi5@gmail.com>
2022-09-12 15:13:04 +0200

    edited main.js file

--- main.js
+++ main.js
@@ -3,24 +3,23 @@ const menuOption = document.querySelector('.menu_opt');
 const closeMenu = document.querySelector('.close_menu');
 
 menu.addEventListener('click', () => {
-menuOption.classList.remove['open'];
+menuOption.classList.remove.open;
 menuOption.style.display='block'; 
 menuOption.style.height='100vh'; 
 menu.style.visibility='hidden';
 closeMenu.style.visibility='visible';
 });
 
-closeMenu.addEventListener('click', ()=>{
-    menuOption.classList.toggle['open'];
-    menuOption.style.height='0';
-    menu.style.visibility='visible';
-    closeMenu.style.visibility='hidden';
+closeMenu.addEventListener('click', () => {
+  menuOption.classList.toggle.open;
+  menuOption.style.height='0';
+  menu.style.visibility='visible';
+  closeMenu.style.visibility='hidden';
 });
 
 menuOption.addEventListener('click', () => {
-    closeMenu.style.visibility='hidden';
-    menuOption.style.display='none';
-    menuOption.style.height='0';
-    menu.style.visibility='visible';
+  closeMenu.style.visibility='hidden';
+  menuOption.style.display='none';
+  menuOption.style.height='0';
+  menu.style.visibility='visible';
 });
-    
