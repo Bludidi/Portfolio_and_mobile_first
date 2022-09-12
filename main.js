07eac38636c3110d15f3391b930be1a65e3d9476
Bludidi <bludidi5@gmail.com>
2022-09-12 23:56:37 +0200

    made changes according to review

--- main.js
+++ main.js
@@ -1,25 +1,45 @@
 const menu = document.querySelector('.menu');
 const menuOption = document.querySelector('.menu_opt');
-const closeMenu = document.querySelector('.close_menu');
+const closeMenu = document.getElementById('exit');
+const menuItems = document.querySelector('.menu_list');
 
-menu.addEventListener('click', () => {
-  menuOption.classList.remove('open');
+// menu.addEventListener('click', () => {
+//   // menuOption.classList.remove('show');
+//   menuOption.style.display = 'block';
+//   menuOption.style.height = '100vh';
+//   menu.style.visibility = 'hidden';
+//   // closeMenu.style.visibility = 'visible';
+// });
+
+// menuOption.addEventListener('click', () => {
+//   closeMenu.style.visibility = 'hidden';
+//   menuOption.style.display = 'none';
+//   menuOption.style.height = '0';
+//   menu.style.visibility = 'visible';
+// });
+
+
+// closeMenu.addEventListener('click', () => {
+//   // menuOption.classList.toggle('show');
+//   menuOption.style.height = '0';
+//   menu.style.visibility = 'visible';
+//   closeMenu.style.visibility = 'hidden';
+// });
+
+menu.addEventListener('click', show);
+closeMenu.addEventListener('click', close);
+menuItems.addEventListener('click', close); 
+
+function show () {
   menuOption.style.display = 'block';
-  menuOption.style.height = '100vh';
-  menu.style.visibility = 'hidden';
-  closeMenu.style.visibility = 'visible';
-});
-
-closeMenu.addEventListener('click', () => {
   menuOption.classList.toggle('open');
-  menuOption.style.height = '0';
-  menu.style.visibility = 'visible';
-  closeMenu.style.visibility = 'hidden';
-});
+  menu.style.visibility ='hidden';
+  // closeMenu.style.visibility ='visible';
+};
 
-menuOption.addEventListener('click', () => {
-  closeMenu.style.visibility = 'hidden';
+function close () {
   menuOption.style.display = 'none';
-  menuOption.style.height = '0';
-  menu.style.visibility = 'visible';
-});
+  menuOption.classList.remove('open');
+  menu.style.visibility ='visible';
+  // closeMenu.style.visibility ='hidden';
+};
