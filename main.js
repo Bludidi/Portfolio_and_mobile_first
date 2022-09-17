5726f3ecb76fdf0bce913d10db29650fa7e8a211
Bludidi <bludidi5@gmail.com>
2022-09-17 09:55:55 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,20 +1,17 @@
 const menu = document.querySelector('.menu');
-const menuOption = document.querySelector('.menu_opt');
+const menuOption = document.querySelector('.menu-opt');
 const closeMenu = document.getElementById('exit');
 const menuItems = document.querySelector('.menu_list');
 
 function show() {
-  menuOption.style.display = 'block';
   menuOption.classList.toggle('open');
   menu.style.visibility = 'hidden';
-  // closeMenu.style.visibility ='visible';
 }
 
 function close() {
   menuOption.style.display = 'none';
   menuOption.classList.remove('open');
   menu.style.visibility = 'visible';
-  // closeMenu.style.visibility ='hidden';
 }
 
 menu.addEventListener('click', show);
